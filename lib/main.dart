8adf9146a95cefe225910cb151fa7f68a5da1cb4
Tushar Jaiswal <104570435+tusharind@users.noreply.github.com>
2024-10-27 21:55:43 +0530

    ch2 done

--- lib/main.dart
+++ lib/main.dart
@@ -4,7 +4,7 @@ void main() {
   runApp(MaterialApp(
     home: HomePage(),
     theme: ThemeData(
-      primarySwatch: Colors.yellow, // Sets the theme's primary color to green
+      primarySwatch: Colors.purple, // Sets the theme's primary color to green
     ),
   ));
 }
@@ -15,13 +15,36 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Awesome App"),
-        // backgroundColor: Colors.blue, // Removed to use the theme's color
-      ),
-      body: Container(
-        child: Text("Hello, Tushar!"),
-      ),
-    );
+        appBar: AppBar(
+          title: Text("Awesome App"),
+          // backgroundColor: Colors.blue, // Removed to use the theme's color
+        ),
+        body: Container(
+          color: Colors.teal,
+          width: 500,
+          child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: <Widget>[
+                Container(
+                    padding: const EdgeInsets.all(8),
+                    color: Colors.red,
+                    width: 100,
+                    height: 100,
+                    child: Text("I am a box")),
+                Container(
+                    padding: const EdgeInsets.all(8),
+                    color: Colors.blue,
+                    width: 100,
+                    height: 100,
+                    child: Text("I am a box")),
+                Container(
+                    padding: const EdgeInsets.all(8),
+                    color: Colors.green,
+                    width: 100,
+                    height: 100,
+                    child: Text("I am a box")),
+              ]),
+        ));
   }
 }
