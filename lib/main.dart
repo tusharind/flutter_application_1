2e55e5e0e490e503049aed974f0cadabc4b286eb
Tushar Jaiswal <104570435+tusharind@users.noreply.github.com>
2024-10-27 17:51:29 +0530

    ch1 completed

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,27 @@
+import 'package:flutter/material.dart';
 
+void main() {
+  runApp(MaterialApp(
+    home: HomePage(),
+    theme: ThemeData(
+      primarySwatch: Colors.yellow, // Sets the theme's primary color to green
+    ),
+  ));
+}
+
+class HomePage extends StatelessWidget {
+  HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Awesome App"),
+        // backgroundColor: Colors.blue, // Removed to use the theme's color
+      ),
+      body: Container(
+        child: Text("Hello, Tushar!"),
+      ),
+    );
+  }
+}
