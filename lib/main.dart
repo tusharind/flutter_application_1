63dcfee9246e2f7cdd6d15c9fafda5ef66071fb5
Tushar Jaiswal <104570435+tusharind@users.noreply.github.com>
2024-10-28 17:35:33 +0530

    here I learnt staeful widgets

--- lib/main.dart
+++ lib/main.dart
@@ -1,50 +1,112 @@
 import 'package:flutter/material.dart';
 
 void main() {
+  // WidgetsApp //MaterialApp //CupertinoApp
   runApp(MaterialApp(
     home: HomePage(),
     theme: ThemeData(
-      primarySwatch: Colors.purple, // Sets the theme's primary color to green
+      primarySwatch: Colors.purple,
     ),
   ));
 }
 
-class HomePage extends StatelessWidget {
-  HomePage({super.key});
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  var myText = "Change My Name";
+  TextEditingController _nameController = TextEditingController();
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Text("Awesome App"),
-          // backgroundColor: Colors.blue, // Removed to use the theme's color
+      backgroundColor: Colors.grey[200],
+      appBar: AppBar(
+        title: Text("Awesome App"),
+      ),
+      body: Center(
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: SingleChildScrollView(
+            child: Card(
+              child: Column(
+                children: <Widget>[
+                  Image.asset(
+                    "assets/3ofus.jpg",
+                  ),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  Text(
+                    myText,
+                    style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
+                  ),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(16.0),
+                    child: TextField(
+                      controller: _nameController,
+                      decoration: InputDecoration(
+                        border: OutlineInputBorder(),
+                        hintText: "Enter Some Text",
+                        labelText: "Name",
+                      ),
+                    ),
+                  )
+                ],
+              ),
+            ),
+          ),
         ),
-        body: Container(
-          color: Colors.teal,
-          width: 500,
-          child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: <Widget>[
-                Container(
-                    padding: const EdgeInsets.all(8),
-                    color: Colors.red,
-                    width: 100,
-                    height: 100,
-                    child: Text("I am a box")),
-                Container(
-                    padding: const EdgeInsets.all(8),
-                    color: Colors.blue,
-                    width: 100,
-                    height: 100,
-                    child: Text("I am a box")),
-                Container(
-                    padding: const EdgeInsets.all(8),
-                    color: Colors.green,
-                    width: 100,
-                    height: 100,
-                    child: Text("I am a box")),
-              ]),
-        ));
+      ),
+      drawer: Drawer(
+        child: ListView(
+          padding: const EdgeInsets.all(0),
+          children: <Widget>[
+            UserAccountsDrawerHeader(
+                accountName: Text("Pawan Kumar"),
+                accountEmail: Text("mtechviral@gmail.com"),
+                currentAccountPicture: CircleAvatar(
+                  backgroundImage: NetworkImage(
+                      "https://images.unsplash.com/photo-1497551060073-4c5ab6435f12?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60"),
+                )),
+            ListTile(
+              leading: Icon(Icons.person),
+              title: Text("Pawan Kumar"),
+              subtitle: Text("Developer"),
+              trailing: Icon(Icons.edit),
+              onTap: () {},
+            ),
+            ListTile(
+              leading: Icon(Icons.email),
+              title: Text("Email"),
+              subtitle: Text("mtechviral@gmail.com"),
+              trailing: Icon(Icons.edit),
+            )
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          myText = _nameController.text;
+          setState(() {});
+        },
+        child: Icon(Icons.send),
+      ),
+    );
   }
 }
